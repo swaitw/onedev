38ee186994d29e5cc4c816f9c37a92fc82500bc4
Robin Shen <robin@onedev.io>
2022-03-10 20:49:10 +0800

    midwork

---
+++ server-core/src/main/java/io/onedev/server/web/component/filterabletree/FilterableTreePanel.java
@@ -0,0 +1,12 @@
+package io.onedev.server.web.component.filterabletree;
+
+import org.apache.wicket.markup.html.panel.Panel;
+
+@SuppressWarnings("serial")
+public class FilterableTreePanel extends Panel {
+
+	public FilterableTreePanel(String id) {
+		super(id);
+	}
+
+}
